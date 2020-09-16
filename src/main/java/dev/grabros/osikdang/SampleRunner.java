e1d0c4f4ce5ee6f28edafc8d24142fb223028941
wooody92 <pkower4@naver.com>
2020-09-16 17:39:14 +0900

    [#2] feat: Restaurant 엔티티 생성 및 Csv 데이터 입력
    - Spring Data JPA를 사용하여 Restaurant 스키마 생성했습니다.
    - Mysql - Csv 데이터 입력했습니다.

---
+++ src/main/java/dev/grabros/osikdang/SampleRunner.java
@@ -0,0 +1,21 @@
+package dev.grabros.osikdang;
+
+import dev.grabros.osikdang.domain.restaurant.Restaurant;
+import dev.grabros.osikdang.domain.restaurant.RestaurantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SampleRunner implements ApplicationRunner {
+
+    @Autowired
+    RestaurantRepository restaurantRepository;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        Restaurant restaurant = new Restaurant();
+//        restaurantRepository.save(restaurant);
+    }
+}
