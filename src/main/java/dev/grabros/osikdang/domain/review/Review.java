650415d2964c431cabeded52c2316618743796c6
wooody92 <pkower4@naver.com>
2020-10-02 15:36:55 +0900

    [#5] feat: User - Review 연관관계 설정
    - User - Review 1:N으로 연관관계 수립
    - FetchType.LAZY으로 설정

--- src/main/java/dev/grabros/osikdang/domain/review/Review.java
+++ src/main/java/dev/grabros/osikdang/domain/review/Review.java
@@ -1,17 +1,22 @@
 package dev.grabros.osikdang.domain.review;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import dev.grabros.osikdang.domain.user.User;
 import java.time.LocalDate;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Entity
 @Getter
-@ToString
+@ToString(exclude = {"user"})
 @NoArgsConstructor
 public class Review {
 
@@ -24,4 +29,9 @@ public class Review {
     private Float rating;
 
     private LocalDate modifiedTime;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 }
--- src/main/java/dev/grabros/osikdang/domain/user/User.java
+++ src/main/java/dev/grabros/osikdang/domain/user/User.java
@@ -1,9 +1,14 @@
 package dev.grabros.osikdang.domain.user;
 
+import dev.grabros.osikdang.domain.review.Review;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -23,4 +28,7 @@ public class User {
     private String email;
 
     private String profile;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Review> reviews = new ArrayList<>();
 }
