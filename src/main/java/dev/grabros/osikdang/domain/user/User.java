38039b57c1bdd07ce00fbe87fc94df126d6b8af4
wooody92 <pkower4@naver.com>
2020-10-02 15:26:38 +0900

    [#5] feat: User entity 생성
    - User Entity 생성했습니다.

---
+++ src/main/java/dev/grabros/osikdang/domain/user/User.java
@@ -0,0 +1,26 @@
+package dev.grabros.osikdang.domain.user;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Getter
+@ToString
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String profile;
+}
