4309fe1f7f40e8cf3cda297cc97b2373f6cce655
wooody92 <pkower4@naver.com>
2020-10-05 15:11:13 +0900

    [#5] feat: MyList entity 생성
    - 나만의 맛집 리스트인 MyList entity 생성

---
+++ src/main/java/dev/grabros/osikdang/domain/myList/MyList.java
@@ -0,0 +1,41 @@
+package dev.grabros.osikdang.domain.myList;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import dev.grabros.osikdang.domain.restaurant.Restaurant;
+import dev.grabros.osikdang.domain.user.User;
+import java.time.LocalDate;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Getter
+@ToString(exclude = {"user", "restaurant"})
+@NoArgsConstructor
+public class MyList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String feed;
+
+    private LocalDate modifiedTime;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "restaurant_id")
+    private Restaurant restaurant;
+}
