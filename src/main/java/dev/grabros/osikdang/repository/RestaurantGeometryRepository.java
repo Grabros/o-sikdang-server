8c979ca378bf340c11bc8831de6b469f4016908e
wooody92 <pkower4@naver.com>
2020-10-08 16:29:45 +0900

    [#7] refactor: native query 요청하는 메서드 Repository로 변경
    - 의미 상 쿼리 요청부분은 서비스 계층이 아닌 레포지토리 계층에서 처리하는게 맞다고 생각되어 변경
    - Spring Data Jpa에서 entityManager로 처리할 수 있는지 확인 필요

---
+++ src/main/java/dev/grabros/osikdang/repository/RestaurantGeometryRepository.java
@@ -0,0 +1,42 @@
+package dev.grabros.osikdang.repository;
+
+import dev.grabros.osikdang.domain.restaurant.Restaurant;
+import dev.grabros.osikdang.geometry.Direction;
+import dev.grabros.osikdang.geometry.GeometryUtil;
+import dev.grabros.osikdang.geometry.Location;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class RestaurantGeometryRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public List<Restaurant> getNearByRestaurants(Double latitude, Double longitude, Double distance) {
+        Location northEast = GeometryUtil
+            .calculate(latitude, longitude, distance, Direction.NORTHEAST.getBearing());
+        Location southWest = GeometryUtil
+            .calculate(latitude, longitude, distance, Direction.SOUTHWEST.getBearing());
+
+        double x1 = northEast.getLatitude();
+        double y1 = northEast.getLongitude();
+        double x2 = southWest.getLatitude();
+        double y2 = southWest.getLongitude();
+
+        String pointFormat = String.format("'LINESTRING(%f %f, %f %f)')", x1, y1, x2, y2);
+        Query query = em.createNativeQuery("SELECT r.id, r.address, r.address_city, "
+            + "r.address_district, r.address_district_old, r.address_old, r.address_province, "
+            + "r.category, r.category_code, r.category_industry, r.category_main, r.category_sub, "
+            + "r.point, r.name, r.zip_code "
+            + "FROM restaurant AS r "
+            + "WHERE MBRContains(ST_LINESTRINGFROMTEXT(" + pointFormat + ", r.point)", Restaurant.class)
+            .setMaxResults(10);
+
+        List<Restaurant> restaurants = query.getResultList();
+        return restaurants;
+    }
+}
