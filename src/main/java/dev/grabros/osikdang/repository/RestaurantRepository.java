02d9a3603e32c00f2e9ac2dc3eb038c00ab3471d
wooody92 <pkower4@naver.com>
2020-10-08 16:03:37 +0900

    [#11] feat: findAllByCategoryMain - 카테고리로 식당 조회
    - 카테고리로 식당 조회하는 메서드 추가

--- src/main/java/dev/grabros/osikdang/repository/RestaurantRepository.java
+++ src/main/java/dev/grabros/osikdang/repository/RestaurantRepository.java
@@ -1,8 +1,10 @@
 package dev.grabros.osikdang.repository;
 
 import dev.grabros.osikdang.domain.restaurant.Restaurant;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 
+    List<Restaurant> findAllByCategoryMain(String category);
 }
