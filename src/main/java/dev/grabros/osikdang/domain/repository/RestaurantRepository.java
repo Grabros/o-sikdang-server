027d5aacb227f9bd38c46226a0840c5cf122f1fd
wooody92 <pkower4@naver.com>
2020-10-16 21:59:57 +0900

    [#11] feat: 페이징 기능을 위해 Repository - Page 타입으로 가져오도록 변경
    - List -> Page 변경

--- src/main/java/dev/grabros/osikdang/domain/repository/RestaurantRepository.java
+++ src/main/java/dev/grabros/osikdang/domain/repository/RestaurantRepository.java
@@ -1,10 +1,11 @@
 package dev.grabros.osikdang.domain.repository;
 
 import dev.grabros.osikdang.domain.entity.restaurant.Restaurant;
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 
-    List<Restaurant> findAllByCategoryMain(String category);
+    Page<Restaurant> findAllByCategoryMain(String category, Pageable pageable);
 }
