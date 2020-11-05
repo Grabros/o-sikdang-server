2c5cbdba2e28567785334765e5d6fe7c51f48b77
wooody92 <pkower4@naver.com>
2020-11-05 18:33:51 +0900

    [#14] config: CORS web-config 설정 추가
    - CORS web-config 설정 override

---
+++ src/main/java/dev/grabros/osikdang/global/config/WebConfig.java
@@ -0,0 +1,16 @@
+package dev.grabros.osikdang.global.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true);
+    }
+}
