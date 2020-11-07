023ee7e7dfd671baf4434355e15f6fc755c3ad05
wooody92 <pkower4@naver.com>
2020-11-07 11:30:14 +0900

    [#14] feat: kakao API 요청을 위한 ENUM 생성
    - kakao API 요청을 위한 ENUM 생성

--- src/main/java/dev/grabros/osikdang/openApi/kakao/KakaoMapEnum.java
+++ src/main/java/dev/grabros/osikdang/openApi/kakao/KakaoMapEnum.java
@@ -1,4 +1,17 @@
 package dev.grabros.osikdang.openApi.kakao;
 
+import lombok.Getter;
+
+@Getter
 public enum KakaoMapEnum {
+    BASE_URL("https://dapi.kakao.com/v2/local/geo/coord2regioncode.json"),
+    INPUT_COORD("input_coord"),
+    OUTPUT_COORD("output_coord"),
+    WGS84("WGS84");
+
+    private String value;
+
+    KakaoMapEnum(String value) {
+        this.value = value;
+    }
 }
