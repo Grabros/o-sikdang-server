f10af2a671402e573a9f62d1d1cec48342cd6071
wooody92 <pkower4@naver.com>
2020-11-07 11:30:54 +0900

    [#14] feat: kakao API 요청을 위한 인터페이스 생성 및 상속
    - DIP 의거하여 상속구조로 클래스 생성

--- src/main/java/dev/grabros/osikdang/openApi/OpenApiProvider.java
+++ src/main/java/dev/grabros/osikdang/openApi/OpenApiProvider.java
@@ -1,6 +1,8 @@
 package dev.grabros.osikdang.openApi;
 
+import org.locationtech.jts.geom.Coordinate;
+
 public interface OpenApiProvider {
 
-
+    <T> T requestAddressByCoordinate(Coordinate coordinate);
 }
--- src/main/java/dev/grabros/osikdang/openApi/kakao/KakaoMapProvider.java
+++ src/main/java/dev/grabros/osikdang/openApi/kakao/KakaoMapProvider.java
@@ -1,7 +1,23 @@
 package dev.grabros.osikdang.openApi.kakao;
 
 import dev.grabros.osikdang.openApi.OpenApiProvider;
+import dev.grabros.osikdang.openApi.kakao.response.KakaoMap;
+import lombok.RequiredArgsConstructor;
+import org.locationtech.jts.geom.Coordinate;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import static dev.grabros.osikdang.openApi.kakao.KakaoMapEnum.*;
+
+@Component
+@RequiredArgsConstructor
 public class KakaoMapProvider implements OpenApiProvider {
 
+    private final WebClient.Builder builder;
+
+    @Override
+    public KakaoMap requestAddressByCoordinate(Coordinate coordinate) {
+        WebClient webClient = builder.baseUrl(BASE_URL.getValue()).build();
+        return null;
+    }
 }
